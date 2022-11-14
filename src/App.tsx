e0138cf1b42ce9aa2a053248b0700408f6bc2089
Sofia Ruban <sophiaruban249324@gmail.com>
2022-11-14 10:48:04 +0200

    add error state

--- src/App.tsx
+++ src/App.tsx
@@ -22,10 +22,11 @@ function  App() {
     [jobIndex]
   )
 
-  const url: string ="https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu"
+  const url: string ="https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu";
 
   const [jobData, setJobData] = useState<JobDataTypes>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(()=> {
       async function fetchData() {
@@ -36,14 +37,15 @@ function  App() {
               setJobData(results);
 
           } catch (error) {
-              console.log(error);
+              setError(true);
           }
           setLoading(false);
       }
       fetchData();
   }, [])
 
-  if (loading) return <div>Loading ...</div>
+  if (loading) return <div className='flex justify-center mt-60 text-3xl'>Loading ...</div>
+  if (error) return <div className='flex justify-center mt-60 text-3xl'>Oops, something went wrong, please try again.</div>
 
   const job = jobData[jobIndex];
 
